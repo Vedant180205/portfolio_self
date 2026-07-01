22d0391815e528c95a4901e3c6fd0f41b4712954
Vedant180205 <vedbhumi123@gmail.com>
2026-07-01 14:34:14 +0530

    chore: update og:image to apple-touch-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     siteName: 'Vedant Patil Portfolio',
     images: [
       {
-        url: '/og-image.jpg',
+        url: '/apple-touch-icon.png',
         width: 1200,
         height: 630,
       }
